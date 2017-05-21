e90f04253a50f701527c2ac29e49f0481c570c97
billchurch <wmchurch@gmail.com>
2017-05-20 21:31:42 -0400

    declaring variable

--- socket/index.js
+++ socket/index.js
@@ -4,7 +4,7 @@ var myError
 module.exports = function (socket, io) {
   this.SSHerror = function (myFunc, err) {
     myError = (myError) || ((err) ? err.message : undefined)
-    thisError = (myError) ? ': ' + myError : ''
+    var thisError = (myError) ? ': ' + myError : ''
     debug('SSH ' + myFunc + thisError)
     socket.emit('ssherror', 'SSH ' + myFunc + thisError)
     socket.disconnect(true)
