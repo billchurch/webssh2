7dc8cfb0f11acce633caedd904d87c9d92535ff0
billchurch <wmchurch@gmail.com>
2018-02-06 10:43:15 -0500

    remove references to host key hashes

--- socket.js
+++ socket.js
@@ -5,7 +5,7 @@ var debug = require('debug')
 var debugWebSSH2 = require('debug')('WebSSH2')
 var SSH = require('ssh2').Client
 var fs = require('fs')
-var hostkeys = JSON.parse(fs.readFileSync('./hostkeyhashes.json', 'utf8'))
+// var hostkeys = JSON.parse(fs.readFileSync('./hostkeyhashes.json', 'utf8'))
 var termCols, termRows
 
 // public
