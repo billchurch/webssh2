ccee67b7e8423b028e983451fc01fc9f2f4875ca
Bill Church <billchurch@users.noreply.github.com>
2019-01-10 09:29:48 -0600

    Issue 125 (#126)
    * fix `npm run build`
    
    fixes #125
    - update font-awesome fonts to 5.6.3
    - update webpack and dependancies
    - update xterm to 3.8.0

--- app/scripts/webpack.common.js
+++ app/scripts/webpack.common.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
