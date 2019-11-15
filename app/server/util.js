edff20939236401e868cb040ff091091bb311d35
Ievgen Varavva <fuzzy.parabola@gmail.com>
2019-11-15 09:40:10 -0800

    Accept default username/password overrides from config. Clarified supplying custom config. (#146)

--- app/server/util.js
+++ app/server/util.js
@@ -7,6 +7,13 @@ require('colors') // allow for color property extensions in log messages
 var debug = require('debug')('WebSSH2')
 var Auth = require('basic-auth')
 
+let defaultCredentials = {username: null, password: null};
+
+exports.setDefaultCredentials = function (username, password) {
+    defaultCredentials.username = username;
+    defaultCredentials.password = password;
+}
+
 exports.basicAuth = function basicAuth (req, res, next) {
   var myAuth = Auth(req)
   if (myAuth && myAuth.pass !== '') {
@@ -15,13 +22,20 @@ exports.basicAuth = function basicAuth (req, res, next) {
     debug('myAuth.name: ' + myAuth.name.yellow.bold.underline +
       ' and password ' + ((myAuth.pass) ? 'exists'.yellow.bold.underline
       : 'is blank'.underline.red.bold))
-    next()
   } else {
+    req.session.username = defaultCredentials.username;
+    req.session.userpassword = defaultCredentials.password;
+  }
+    console.log("req.session: " +req.session.username + " pass: " + req.session.userpassword);
+    console.log("def: " +defaultCredentials.username + " pass: " + defaultCredentials.password);
+  if (!req.session.userpassword) {
     res.statusCode = 401
     debug('basicAuth credential request (401)')
     res.setHeader('WWW-Authenticate', 'Basic realm="WebSSH"')
     res.end('Username and password required for web SSH service.')
+    return
   }
+  next()
 }
 
 // takes a string, makes it boolean (true if the string is true, false otherwise)
