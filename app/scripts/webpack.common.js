0e6bb2c7dc6a5262569dd5000f81b9c818e61052
Bill Church <wmchurch@gmail.com>
2020-03-14 13:17:10 -0400

    chore: standardjs run

--- app/scripts/webpack.common.js
+++ app/scripts/webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-/* const CleanWebpackPlugin = require('clean-webpack-plugin') */
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 module.exports = {
