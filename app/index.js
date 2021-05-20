f36af08d3684db5b02f53a2a33b1bdb7eabe55b3
Bill Church <wmchurch@gmail.com>
2021-05-20 16:32:57 -0400

    chore: remove auto port increment

--- app/index.js
+++ app/index.js
@@ -15,14 +15,5 @@ server.listen({ host: config.listen.ip, port: config.listen.port });
 console.log(`WebSSH2 service listening on ${config.listen.ip}:${config.listen.port}`);
 
 server.on('error', (err) => {
-  if (err.code === 'EADDRINUSE') {
-    config.listen.port += 1;
-    console.error(`WebSSH2 Address in use, retrying on port ${config.listen.port}`);
-    setTimeout(() => {
-      server.listen(config.listen.port);
-    }, 250);
-  } else {
-    // eslint-disable-next-line no-console
-    console.log(`WebSSH2 server.listen ERROR: ${err.code}`);
-  }
+  console.error(`WebSSH2 server.listen ERROR: ${err.code}`);
 });
