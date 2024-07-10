533f719ccad36adb688840ae94540d038d5d2eb9
Bill Church <billchurch@users.noreply.github.com>
2024-07-10 13:09:24 +0000

    chore: dev build testing

--- app/scripts/webpack.common.js
+++ app/scripts/webpack.common.js
@@ -1,41 +1,57 @@
-const webpack = require('webpack')
-const path = require('path')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require("webpack");
+const { BannerPlugin } = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const packageJson = require("../package.json"); // Load package.json
+const commitHash = require("child_process")
+  .execSync("git rev-parse --short HEAD")
+  .toString()
+  .trim();
+
 module.exports = {
-  context: path.resolve('__dirname', '../'),
+  context: path.resolve(__dirname, "../"),
   entry: {
-    webssh2: './client/src/js/index.js'
+    webssh2: "./client/src/js/index.js",
   },
   plugins: [
-    new CleanWebpackPlugin(['client/public'], {
-      root: path.resolve('__dirname', '../'),
-      verbose: true
+    new BannerPlugin({
+      banner: `Version ${packageJson.version} - ${new Date().toISOString()}`,
+      include: /\.(js|css|html|htm)$/,
+    }),
+    new CleanWebpackPlugin(["client/public"], {
+      root: path.resolve("__dirname", "../"),
+      verbose: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./client/src/client.htm", // Path to your source template
+      filename: "client.htm", // Optional: output file name, defaults to index.html
+      minify: false,
+      scriptLoading: "blocking",
+      version: `Version ${
+        packageJson.version
+      } - ${new Date().toISOString()} - ${commitHash}`,
     }),
     new CopyWebpackPlugin([
-      './client/src/client.htm',
-      './client/src/favicon.ico'
+      { from: "./client/src/favicon.ico", to: "favicon.ico" },
     ]),
-    new ExtractTextPlugin('[name].css')
+    new ExtractTextPlugin("[name].css"),
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, '../client/public')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "../client/public"),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader'
-            }
-          ]
-        })
-      }
-    ]
-  }
-}
+          fallback: "style-loader",
+          use: [{ loader: "css-loader" }],
+        }),
+      },
+    ],
+  },
+};
